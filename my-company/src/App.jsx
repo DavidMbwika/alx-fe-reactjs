269bd21c7688854bf465ad6b93a6a0f947671496
DavidMbwika <davidmbwikanite@gmail.com>
2024-11-17 21:48:53 +0300

    props

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import  { BrowserRouter as Routes, Route} from "react-router-dom";
 import Home from './Home.jsx'
 import Contact from './assets/Contact.jsx'
 import About from './assets/About.jsx'
@@ -13,14 +13,12 @@ function App() {
 
   return (
     <>
-        <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </Router>
       <div>
       <Home/>
       <Contact/>
