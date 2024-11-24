24f69181c5390987f18a5ef59ef443d77e596606
DavidMbwika <davidmbwikanite@gmail.com>
2024-11-24 23:29:53 +0300

    three

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -2,8 +2,8 @@
 // components/ProtectedRoute.jsx
 import { Navigate } from 'react-router-dom';
 
-function ProtectedRoute({ isAuth, children }) {
-  return isAuth ? children : <Navigate to="/" />;
+function ProtectedRoute({ useAuth, children }) {
+  return useAuth ? children : <Navigate to="/" />;
 }
 
 export default ProtectedRoute;
