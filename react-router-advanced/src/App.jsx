7ae61902f93f6ef287b526cb35d35ffaf09b878c
DavidMbwika <davidmbwikanite@gmail.com>
2024-11-24 23:32:39 +0300

    three

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ProtectedRoute from './ProtectedRoute';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import BlogPost from './pages/BlogPost';
 
 function App() {
   return (
+    <>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
@@ -12,6 +14,8 @@ function App() {
         <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </BrowserRouter>
+    <ProtectedRoute/>
+  </>
   );
 }
 
