bd889a549025e31d76221f661bda0726e4a60bc9
DavidMbwika <davidmbwikanite@gmail.com>
2024-12-01 16:17:03 +0300

    tailwind

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import { useState } from 'react'
+import UserProfile from "./components/UserProfile";
 import './App.css'
 
 function App() {
@@ -7,9 +8,13 @@ function App() {
   return (
     <>
     <div className="text-center">
-      <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind!</h1>
+      <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind!
+        THIS IS MY PROJECT
+      </h1>
     </div>
-
+    <div>
+      <UserProfile />
+    </div>
     </>
   )
 }
