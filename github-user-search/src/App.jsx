caf0a80206e9933154b6d29c6787607841ccd271
DavidMbwika <davidmbwikanite@gmail.com>
2024-12-15 20:37:02 +0300

    finished

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <h1>GitHub User Search</h1>
+      </header>
+      <main>
+        <p>Start searching for GitHub users here!</p>
+      </main>
+    </div>
+  );
+};
+
+export default App;
